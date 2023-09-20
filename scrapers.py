463d1e9c1e4bd8ea39821f1b437f9a1465e49cf7
luisedluised <eduardoabprev@gmail.com>
2023-09-20 04:46:16 -0300

    features: price range, region

--- scrapers.py
+++ scrapers.py
@@ -11,16 +11,35 @@ from IPython.display import Image, display, HTML
 user_agent_list = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.0.0 Safari/537.36',
                    'Chrome/79.0.3945.130']
 
+
+country_strings = {
+    'EU': '&countryIds=DE&countryIds=BE&countryIds=FI&countryIds=PT&countryIds=BG&countryIds=DK&countryIds=LT&countryIds=LU&countryIds=HR&countryIds=LV&countryIds=FR&countryIds=HU&countryIds=SE&countryIds=SI&countryIds=SK&countryIds=IE&countryIds=EE&countryIds=MQ&countryIds=MT&countryIds=GP&countryIds=GR&countryIds=IT&countryIds=ES&countryIds=AT&countryIds=CY&countryIds=CZ&countryIds=PL&countryIds=RO&countryIds=NL',
+    'US': '&countryIds=US&',
+    'BR': '&countryIds=BR',
+    '':''
+}
+
 def clean_string(string):
     return unidecode(''.join([i for i in string if i.isalpha()]).lower())
 
-def format_query_url(brand, model, size_range, material, maxprice, condition, page_number):
+def format_query_url(query, page_number):
+    brand = query['brand']
+    model = query['model']
+    size_range = query['size_range'] if 'size_range' in query else []
+    material = query['material'] if 'material' in query else ''
+    condition = query['condition'] if 'condition' in query else ''
+    region = query['region'] if 'region' in query else ''
+    price_range = query['price_range'] if 'price_range' in query else [None, None]
+    
     brand = brand.replace(' ', '')
     model = (brand + ' ' + model).replace(' ', '+')
     page_number = '&showpage={}'.format(page_number)
 
-    condition = '&usedOrNew={}'.format(condition)   if condition != '' else ''
-    maxprice = '&priceTo={}'.format(maxprice)       if maxprice != '' else ''
+    condition = '&usedOrNew={}'.format(condition)     if condition != '' else ''
+    minprice = '&priceFrom={}'.format(price_range[0]) if price_range[0] not in ('', None) else ''
+    maxprice = '&priceTo={}'.format(price_range[1])   if price_range[1] not in ('', None) else ''
+
+    region = country_strings[region]
 
     material = 3 if material == 'gold' else 4 if material == 'steel' else ''
     material = '&caseMaterials={}'.format(material) if material != '' else ''
@@ -36,20 +55,13 @@ def format_query_url(brand, model, size_range, material, maxprice, condition, pa
             sizes += '&caseDiameter={}'.format(i)
 
     url = f'https://www.chrono24.com.br/{brand}/index.htm?'\
-        f'{sizes}{material}dosearch=true&pageSize=120query={model}{page_number}{condition}{maxprice}'
+        f'{region}{sizes}{material}dosearch=true&pageSize=120query={model}{page_number}{condition}{minprice}{maxprice}'
     return url
 
 
-def search_watch(brand, model, size_range, material, maxprice, condition, page_number):
-    model_brand = brand + ' ' + model
-    url = 'https://www.chrono24.com.br/{}/index.htm?'\
-        'caseDiameter=34&caseDiameter=35&caseDiameter=36&'\
-        'caseMaterials=3'\
-        'dosearch=true&pageSize=120'\
-        '&query={}&showpage={}&usedOrNew={}&priceTo={}'.format(
-        brand.replace(' ', ''), model_brand.replace(' ', '+'), page_number, condition, maxprice)
-    
-    url = format_query_url(brand, model, size_range, material, maxprice, condition, page_number)
+def search_watch(query, page_number):
+
+    url = format_query_url(query, page_number)
     headers = {
         'User-Agent': user_agent_list[0],
         'Accept-Language': 'en-US,en;q=0.9'}
@@ -99,8 +111,8 @@ def format_search_results_to_dataframe(json_data):
     return offer_frame.sort_values('price')
 
 
-def get_page(brand, model, size_range, material, maxprice, condition, page_number):
-    response = search_watch(brand, model, size_range, material, maxprice, condition, page_number)
+def get_page(query, page_number):
+    response = search_watch(query, page_number)
     html_element = extract_html_element(response, element = 'application/ld+json')
     json_data = convert_string_to_json(html_element)
     offer_frame = format_search_results_to_dataframe(json_data)
@@ -128,20 +140,18 @@ def get_offer_information(url):
     out['url'] = url
     return out
 
-def clean_frame(df, max_price, model):
+def clean_frame(df, model):
     df['clean_name'] = df.name.apply(clean_string)
-    df = df[df.clean_name.str.contains(clean_string(model))]
-    df = df[df.price <= max_price].drop('clean_name', axis=1)
+    df = df[df.clean_name.str.contains(clean_string(model))].drop('clean_name', axis=1)
     return df
 
 
-def search_multiple_pages(brand, model, size_range, material, maxprice, condition, max_pages = 1000, clean_by_name = False):
+def search_multiple_pages(query, name_enforce, max_pages):
     offer_frame = pd.DataFrame()
     page_number = 1
     while True:
-
-        new_rows =  get_page(brand, model, size_range, material, maxprice, condition, page_number)
-        if clean_by_name: new_rows = clean_frame(new_rows.copy(), maxprice, model)
+        new_rows =  get_page(query, page_number)
+        if name_enforce: new_rows = clean_frame(new_rows.copy(), query['model'])
         new_rows['page'] = page_number
         previous_size = offer_frame.shape[0]
         offer_frame = pd.concat([offer_frame, new_rows])
@@ -156,17 +166,15 @@ def search_multiple_pages(brand, model, size_range, material, maxprice, conditio
 
 
 
-
-def show_deals(queries, max_price, max_results = 12):
+def show_deals(queries, max_results = 12, img_height = 200, name_enforce = True, hard_max_price = np.inf):
     for query in queries:
 
-        brand = query['brand']
-        model = query['model']
-        size_range = query['size_range'] if 'size_range' in query else []
-        material = query['material'] if 'material' in query else ''
-        condition = query['condition'] if 'condition' in query else ''
 
-        results = search_multiple_pages(brand, model, size_range, material, max_price, condition, clean_by_name = True).head(max_results)
+
+        results = search_multiple_pages(query, name_enforce, max_pages = 50
+                                        ).head(max_results)
+        results = results[results.price <= hard_max_price]
+        
         bounds = np.arange(0, max_results + 1, 4)
         for i in range(len(bounds) - 1):
             a = bounds[i]
@@ -180,10 +188,10 @@ def show_deals(queries, max_price, max_results = 12):
                 html_code += f"""
                 <div style="flex: 1; margin-right: 10px;">
                     <a href="{row.url}" target="_blank">
-                        <img src="{row.image}" style="height: 200px; width: auto" />
+                        <img src="{row.image}" style="height: {img_height}px; width: auto" />
                     </a>
                     <div style="margin-top: 10px;">
-                        <p>R${row.price}, {row['name'][:30]}</p>
+                        <p>R${row.price}, {row['name'][:25]}</p>
                     </div>
                 </div>
                 """
